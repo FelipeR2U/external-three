74164e8816ccda1556841a1f5bea0677258afab1
FelipeR2U <felipe@r2u.io>
2021-09-08 11:52:40 -0300

    Add comment of solution

--- project/src/test.ts
+++ project/src/test.ts
@@ -1,3 +1,17 @@
 import { createScene } from 'library'
 
 createScene()
+
+/* Solution
+ 
+import * as mod from 'library'
+
+const run = async () => {
+  const test = await mod
+
+  test.createScene()
+}
+
+run()
+
+*/
